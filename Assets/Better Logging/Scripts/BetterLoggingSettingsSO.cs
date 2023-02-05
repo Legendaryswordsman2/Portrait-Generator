a4b8572de28d8d8cfcf72f578cdfba4f83d9dd93
LegendarySwordsman2 <87250196+Legendaryswordsman2@users.noreply.github.com>
2023-02-05 01:44:39 -0800

    Update BetterLoggingSettingsSO.cs

--- Assets/Better Logging/Scripts/BetterLoggingSettingsSO.cs
+++ Assets/Better Logging/Scripts/BetterLoggingSettingsSO.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Better Logging Settings")]
 public class BetterLoggingSettingsSO : ScriptableObject
 {
     [Header("Settings")]
