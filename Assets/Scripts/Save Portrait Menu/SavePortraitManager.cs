3853683a00e0305197c489a6b800c1ac07f7739c
LegendarySwordsman2 <87250196+Legendaryswordsman2@users.noreply.github.com>
2023-02-06 21:05:23 -0800

    s

---
+++ Assets/Scripts/Save Portrait Menu/SavePortraitManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SavePortraitManager : MonoBehaviour
+{
+    [SerializeField] TMP_InputField fileNameInputField;
+    [SerializeField] TMP_Dropdown sizeDropdown;
+}
