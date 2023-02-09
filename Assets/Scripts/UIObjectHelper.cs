99e3a47edca8863e9d5e636acb88fb2869fd6d91
LegendarySwordsman2 <87250196+Legendaryswordsman2@users.noreply.github.com>
2023-02-09 00:06:39 -0800

    Added loading icons to save menu

---
+++ Assets/Scripts/UIObjectHelper.cs
@@ -0,0 +1,49 @@
+using Cysharp.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.iOS.Extensions.Common;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class UIObjectHelper : MonoBehaviour
+{
+    [SerializeField] TMP_Text text;
+
+    Selectable uiObject;
+
+    Color defaultTextColor;
+
+    private void Awake()
+    {
+        uiObject = GetComponent<Selectable>();
+
+        defaultTextColor = text.color;
+
+        if(uiObject.interactable)
+        WaitUntilDropdownDisabled();
+        else
+            WaitUntilDropdownEnabled();
+    }
+
+    async void WaitUntilDropdownEnabled()
+    {
+        await UniTask.WaitUntil(() => uiObject.interactable == true);
+
+        text.color = defaultTextColor;
+
+        WaitUntilDropdownDisabled();
+    }
+
+    async void WaitUntilDropdownDisabled()
+    {
+        await UniTask.WaitUntil(() => uiObject.interactable == false);
+
+        Color disabledColor = defaultTextColor;
+        disabledColor.a = uiObject.colors.disabledColor.a;
+        text.color = disabledColor;
+
+        WaitUntilDropdownEnabled();
+    }
+}
