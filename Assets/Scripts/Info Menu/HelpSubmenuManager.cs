8e5f0f5a7d478da27d6efa2fbd838a6b258e86b5
LegendarySwordsman2 <87250196+Legendaryswordsman2@users.noreply.github.com>
2023-02-19 22:04:46 -0800

    Added Help Menu (Currently not Functional)

---
+++ Assets/Scripts/Info Menu/HelpSubmenuManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpSubmenuManager : MonoBehaviour
+{
+  public void JoinDiscord()
+    {
+        Application.OpenURL("https://discord.com/invite/2wB3RuAESb");
+    }
+}
