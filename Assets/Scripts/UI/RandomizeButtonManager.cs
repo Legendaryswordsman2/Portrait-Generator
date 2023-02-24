a917da7abbd4f516a37dd5abc728083823a9c0ab
LegendarySwordsman2 <87250196+Legendaryswordsman2@users.noreply.github.com>
2023-02-24 04:29:43 -0800

    Added Animation to Randomize Button
    Also added Leantween library

---
+++ Assets/Scripts/UI/RandomizeButtonManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomizeButtonManager : MonoBehaviour
+{
+    [SerializeField] PGManager pgManager;
+
+    public void RandomizePortrait()
+    {
+        pgManager.RandomizePortrait();
+
+        LeanTween.cancel(gameObject);
+        transform.rotation = Quaternion.identity;
+        LeanTween.rotateAround(gameObject, new Vector3(0, 0, 360), 360, 0.4f).setEaseInOutBack();
+
+    }
+}
